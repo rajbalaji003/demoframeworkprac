cdd999d3402324ed2a14a60bd2163f408a336a1e
rajbalaji003 <rajbalaji003@gmail.com>
2025-03-13 11:27:48 +0530

    second commit added contact

---
+++ demoframeworkprac2/src/test/java/com/crm/comcast/contacttest/createContactTest.java
@@ -0,0 +1,21 @@
+package com.crm.comcast.contacttest;
+
+
+import org.testng.annotations.Test;
+
+import com.crm.genericbaseutility.BaseClass;
+
+public class createContactTest  extends BaseClass {
+	
+	@Test
+	public void createContact() {
+		System.out.println("execute createContact & verify");
+		
+	}
+	@Test
+	public void createContactWithSupportDate() {
+		System.out.println("execute createContactWithSupportDate & verify");
+	}
+	
+
+}
