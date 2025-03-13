49879fe6d7485822c526922f37b24bb50fe4df4f
rajbalaji003 <rajbalaji003@gmail.com>
2025-03-13 12:43:03 +0530

    commit third createcontactwithphone

--- demoframeworkprac2/src/test/java/com/crm/comcast/contacttest/createContactTest.java
+++ demoframeworkprac2/src/test/java/com/crm/comcast/contacttest/createContactTest.java
@@ -1,18 +1,18 @@
 package com.crm.comcast.contacttest;
 
 
-import org.testng.annotations.Test;
 
-import com.crm.genericbaseutility.BaseClass;
 
-public class createContactTest  extends BaseClass {
+
+
+public class createContactTest   {
 	
-	@Test
+	
 	public void createContact() {
 		System.out.println("execute createContact & verify");
 		
 	}
-	@Test
+
 	public void createContactWithSupportDate() {
 		System.out.println("execute createContactWithSupportDate & verify");
 	}
---
+++ demoframeworkprac2/src/test/java/com/crm/comcast/contacttest/createContactwithPhoneNumber.java
@@ -0,0 +1,5 @@
+package com.crm.comcast.contacttest;
+
+public class createContactwithPhoneNumber {
+
+}
